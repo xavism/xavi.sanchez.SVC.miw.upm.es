616eb628c56fcd8008332f32352d4711250bcd0b
Xavi Sanchez Mir <xavi.sanchez.mir@gmail.com>
2017-09-29 13:52:17 +0200

    Test C21

---
+++ src/test/java/xavi/sanchez/SVC/miw/upm/es/TestC21.java
@@ -0,0 +1,26 @@
+package xavi.sanchez.SVC.miw.upm.es;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestC21 {
+
+	private C21 obj;
+	
+	@Before
+    public void before() {
+        obj = new C21();
+    }
+	
+	@Test
+	public void testM1() {
+		assertEquals("m1", obj.m1());
+	}
+	
+	@Test
+	public void testM2() {
+		assertEquals("m2", obj.m2());
+	}
+
+}
