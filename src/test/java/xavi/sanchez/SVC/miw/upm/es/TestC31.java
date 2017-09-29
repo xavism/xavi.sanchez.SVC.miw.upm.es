fb0adc8c12dbb88ea7c929d217637235cf8db95f
Xavi Sanchez Mir <xavi.sanchez.mir@gmail.com>
2017-09-29 14:06:31 +0200

    C31Test

---
+++ src/test/java/xavi/sanchez/SVC/miw/upm/es/TestC31.java
@@ -0,0 +1,26 @@
+package xavi.sanchez.SVC.miw.upm.es;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestC31 {
+
+	private C31 obj;
+	
+	@Before
+    public void before() {
+        obj = new C31();
+    }
+	
+	@Test
+	public void testM1() {
+		assertEquals("m1", obj.m1());
+	}
+	
+	@Test
+	public void testM2() {
+		assertEquals("m2", obj.m2());
+	}
+
+}
